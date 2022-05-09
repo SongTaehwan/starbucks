5b6ce38e5eef894bb6e26db7e566fa7c78aa2973
Song TaeHwan <axoghks@gmail.com>
2022-05-09 19:22:31 +0900

    build: Initialize iOS project with dependecies

---
+++ Starbucks/Starbucks/Presentation/Favourites/FavouriteController.swift
@@ -0,0 +1,15 @@
+//
+//  FavouriteController.swift
+//  Starbucks
+//
+//  Created by 송태환 on 2022/05/09.
+//
+
+import UIKit
+
+class FavouriteController: UIViewController {
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    self.view.backgroundColor = .systemBackground
+  }
+}
---
+++ Starbucks/Starbucks/Presentation/Home/HomeController.swift
@@ -0,0 +1,15 @@
+//
+//  HomeViewController.swift
+//  Starbucks
+//
+//  Created by 송태환 on 2022/05/09.
+//
+
+import UIKit
+
+class HomeController: UIViewController {
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    self.view.backgroundColor = .white
+  }
+}
